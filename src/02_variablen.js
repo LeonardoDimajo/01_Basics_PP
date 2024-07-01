53c2f52211506e73176e3dce0025939de11f2e6e
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-01 10:25:24 +0000

    02/ 02 Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -4,5 +4,20 @@
  * 
  *  Block-Kommentar
  * 
- * 
+ *
+ *  
  */
+
+/***** Variablen 01 *******/
+
+// console.log("Hi");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
