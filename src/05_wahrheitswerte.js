001cb0442cd14b82ae36ce7f60e7a62858e22fc1
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-03 07:22:27 +0000

    05/02 Berechung Alter Iteration 3

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -13,13 +13,18 @@ const birthYearMark = 1990;
 
 
 // Berechnung Alter / iteration 2
-let date = new Date();
-let year = date.getFullYear()
-console.log("Jahr " + date.getFullYear())
+// let date = new Date();
+// let year = date.getFullYear()
+// console.log("Jahr " + date.getFullYear())
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter / Iteration 3
+let year = new Date().getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
 
