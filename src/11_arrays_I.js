39de275ac4b6f132b39a10ff8e982cce4a04bb77
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-08 10:02:02 +0000

    11/02 Transponierung

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -41,6 +41,58 @@
 // }
 
 
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+// //    output(i);       // interner Index
+//    output(arr[i]);    // Mapping intern --> extern
+// }
+
+
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   a += 1; 
+// a = a + 1
+   output("inhalt von a: " + a);
+}
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   str += "Test" + " "; // a = a + 1
+   output(str);
+}
+
+
+
 /**
  *   PRAXIS
  * 
@@ -95,3 +147,24 @@ function getSentenceArr(arr) {
 
     return str;
 }
+
+
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+
+// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
+// function getSentenceArr2(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str;
+//     for (let i = 0; i < arr.length; i++) 
+//     {
+//     //    output(i);       // interner Index
+//     output(arr[i]);    // Mapping intern --> extern
+//     }
+
+//     return str;
+// }
